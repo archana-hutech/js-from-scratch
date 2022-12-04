b3576e8d7a61334e751daa5bc69cfecf155705db
Kiran <kiranpoojary483@gmail.com>
2022-12-04 22:04:16 +0530

    product import poc

--- poc/product_import.js
+++ poc/product_import.js
@@ -1,12 +1,24 @@
-// const csv = require("csvtojson");
 import csv from "csvtojson";
-function productCsvFormatter() {
+function productCsvToJsonFormatter() {
   const csvFilePath = "./Copy_of_product_template.csv";
   csv()
     .fromFile(csvFilePath)
-    .then((jsonObj) => {
-      console.log(jsonObj);
+    .then((products) => {
+      let p = [];
+      products.forEach((product, i, allProducts) => {
+        if (i === 0) {
+          p.push(product);
+        } else {
+          for (let key in product) {
+            if (!product[key] && product[i - 1]?.id != p[i - 1]?.id) {
+              product[key] = allProducts[i - 1]?.[key];
+            }
+          }
+          p.push(product);
+        }
+      });
+      console.log(p);
     });
 }
 
-productCsvFormatter();
+productCsvToJsonFormatter();
